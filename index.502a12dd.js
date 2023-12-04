69867633f10f95a6119f8ca21b30d8d5f4e8edf9
FlowMaster83 <110739720+FlowMaster83@users.noreply.github.com>
2023-12-04 18:13:09 +0000

    Deploying to gh-pages from @ FlowMaster83/backend-using@9838ed656f75f4e3cd2f56a631325cdb49d976b5 🚀

---
+++ index.502a12dd.js
@@ -0,0 +1,2 @@
+setTimeout((function(){console.log("2 seconds")}),2e3);
+//# sourceMappingURL=index.502a12dd.js.map
---
+++ index.67fcebe6.js
@@ -0,0 +1,2 @@
+setTimeout((()=>{console.log("2 seconds")}),2e3);
+//# sourceMappingURL=index.67fcebe6.js.map
