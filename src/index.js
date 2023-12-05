57ed94271b3aa175adaf7d235a4042c34aaa6881
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2023-12-05 18:31:59 +0200

    reedit for documentation

--- src/index.js
+++ src/index.js
@@ -48,3 +48,68 @@ DELETE - УДАЛЕНИЕ
 
 POSTMAN - отдаст, но не факт, что будет работать через fetch.
 */
+
+//==============
+
+// Пагинация
+
+// const BASE_URL = 'https://the-one-api.dev/v2';
+// const END_POINT = 'character';
+// const KEY = 'pTksQP-kRtSZO_M71kE_';
+
+// function getCharacter() {
+//   const param = new URLSearchParams({
+//     limit: 30,
+//     page: 1,
+//   });
+
+//   //   опция для типа запроса
+//   const option = {
+//     method: 'GET',
+//     headers: {
+//       Authorization: `Bearer ${KEY}`,
+//     },
+//   };
+
+//   fetch(`${BASE_URL}${END_POINT}?${param}`, option).then(response =>
+//     console.log(response)
+//   );
+// }
+
+// getCharacter();
+
+//==============
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const END_POINT = 'trending/movie/day';
+const API_KEY = '155ac852b40c3d4bc41678b5b0356daa';
+const list = document.querySelector('.js-list');
+
+function getTrending() {
+  fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=20`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  });
+}
+
+getTrending()
+  .then(data =>
+    list.insertAdjacentHTML('beforeend', createMarkup(data.results))
+  )
+  .catch(err => console.log(err));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ poster_path, title }) => `<li>
+    <img  src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}">
+    <h2>${title}</h2>
+  </li>`
+    )
+    .join('');
+}
+
+//==============
